0a3b2a87d9861b7880704ac96a9553a3eaa766be
Vuyisile <vtlehola23@gmail.com>
2024-07-17 13:46:40 +0200

    fix cors error

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const morgan = require('morgan'); // import morgan
+const morgan = require('morgan');
 
 const connectDB = require("./config/db_conn");
 const userRoute = require("./routes/user");
@@ -10,15 +10,23 @@ const passwordHintRoute = require("./routes/password_hint");
 const applicationPasswordsRoute = require("./routes/application_passwords");
 connectDB();
 
-app.use(express.static("public"));
-app.use(express.static("assets"));
-app.use(express.static("node_modules"));
+// app.use(express.static("public"));
+// app.use(express.static("assets"));
+// app.use(express.static("node_modules"));
 app.use(cors({ 
   origin: ["https://absa-password-vault-client.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
  }));
+ // Handle preflight requests
+app.options('*', cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://absa-password-vault-client.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.use(
   express.urlencoded({
     extended: true,
